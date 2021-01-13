2166ca338f6391bdd2cd478801679aa786bfa137
rumy14 <unitedit21@gmail.com>
2021-01-13 16:11:47 +0600

    first commit

---
+++ app/src/main/java/com/example/loginregister1/login_activity.java
@@ -0,0 +1,133 @@
+package com.example.loginregister1;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class login_activity extends AppCompatActivity {
+
+    private ImageView logo, ivSignIn, btnTwitter;
+    private AutoCompleteTextView email, password;
+    private TextView forgotPass, signUp;
+    private Button btnSignIn;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseUser user;
+    private ProgressDialog progressDialog;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_activity);
+
+        initializeGUI();
+
+        user = firebaseAuth.getCurrentUser();
+
+        if(user != null) {
+            finish();
+            startActivity(new Intent(login_activity.this,MainActivity.class));
+        }
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String inEmail = email.getText().toString();
+                String inPassword = password.getText().toString();
+
+                if(validateInput(inEmail, inPassword)){
+                    signUser(inEmail, inPassword);
+                }
+
+            }
+        });
+
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(login_activity.this,RegistrationActivity.class));
+            }
+        });
+
+        forgotPass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(login_activity.this,PWresetActivity.class));
+            }
+        });
+
+
+
+    }
+
+
+
+    public void signUser(String email, String password){
+
+        progressDialog.setMessage("Verificating...");
+        progressDialog.show();
+
+        firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    progressDialog.dismiss();
+                    Toast.makeText(login_activity.this,"Login Successful",Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(login_activity.this,MainActivity.class));
+                }
+                else{
+                    progressDialog.dismiss();
+                    Toast.makeText(login_activity.this,"Invalid email or password",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+
+    private void initializeGUI(){
+
+        logo = findViewById(R.id.ivLogLogo);
+        ivSignIn = findViewById(R.id.ivSignIn);
+        btnTwitter = findViewById(R.id.ivFacebook);
+        email = findViewById(R.id.atvEmailLog);
+        password = findViewById(R.id.atvPasswordLog);
+        forgotPass = findViewById(R.id.tvForgotPass);
+        signUp = findViewById(R.id.tvSignIn);
+        btnSignIn = findViewById(R.id.btnSignIn);
+        progressDialog = new ProgressDialog(this);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+    }
+
+
+    public boolean validateInput(String inemail, String inpassword){
+
+        if(inemail.isEmpty()){
+            email.setError("Email field is empty.");
+            return false;
+        }
+        if(inpassword.isEmpty()){
+            password.setError("Password is empty.");
+            return false;
+        }
+
+        return true;
+    }
+}
